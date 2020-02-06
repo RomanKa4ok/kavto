a801d6e53007a621970c51bd551ac1aeb7d2f525
Romanus <dayoff.in.poltava@gmail.com>
2020-02-06 19:36:57 +0200

    commit

--- php/create_admin.php
+++ php/create_admin.php
@@ -3,11 +3,9 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once 'db_connection.php';
 $dbh = connectToDb();
-$username = 'admin';
-$password = password_hash('password',PASSWORD_DEFAULT);
+$username = 'bkvasha05';
+$password = password_hash('katanzhek055',PASSWORD_DEFAULT);
 $stmt = $dbh->prepare("INSERT INTO admins (username, password) VALUES (?, ?)");
 $stmt->bindParam(1, $username);
 $stmt->bindParam(2, $password);
-
-
 $stmt->execute();
